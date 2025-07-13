ee8eff5482b470994f6fa51f8666a0d2351e5dbd
Kenil Wala <kenilwala@gmail.com>
2025-07-13 23:09:01 +0530

    Learned Syntax of Class and Methods and made a Small Brand and its Speed Calling

---
+++ Day5_Class.java
@@ -0,0 +1,10 @@
+public class Day5_Class {
+    // properties (fields)
+    String brand;
+    int speed;
+
+    // method
+    void drive() {
+        System.out.println(brand + " is driving at " + speed + " km/h");
+    }
+}
---
+++ Day5_Methods.java
@@ -0,0 +1,9 @@
+public class Day5_Methods {
+    public static void main(String[] args) {
+        Day5_Class myCar = new Day5_Class();  // object creation
+        myCar.brand = "BMW";
+        myCar.speed = 300;
+
+        myCar.drive();
+    }
+}
